8c21ae5d0a653fd00a209e6b82f2a538d0d4c73e
young5097 <bigfaith0301@naver.com>
2023-11-15 09:49:14 +0900

    메인페이지 생성

--- APTITUDE/urls.py
+++ APTITUDE/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from main_page import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("main_page.urls")),
 ]
