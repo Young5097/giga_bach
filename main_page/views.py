d3827590ef3095e340e055ce421e128e7ebda60e
devdnwl <133637966+devdnwl@users.noreply.github.com>
2023-11-15 09:49:01 +0900

    startj

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(
+        request,
+        'main_page/index.html'
+    )
+
+def login(request):
+    return render(
+        request,
+        'main_page/login.html'
+    )
+
+def register(request):
+    return render(
+        request,
+        'main_page/register.html'
+    )
