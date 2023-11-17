a508a3483e0712305e34ec5ba4ece5260e17a628
young5097 <bigfaith0301@naver.com>
2023-11-17 09:41:36 +0900

    -

--- main_page/forms.py
+++ main_page/forms.py
@@ -2,13 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class SignUpForm(UserCreationForm):
-    name = forms.CharField(label='성명', max_length=30, required=True, help_text='이름을 입력하세요.')
-    email = forms.EmailField(max_length=254, help_text='유효한 이메일 주소를 입력하세요.')
+    name = forms.CharField(
+        label="성명", max_length=30, required=True, help_text="이름을 입력하세요."
+    )
+    email = forms.EmailField(max_length=254, help_text="유효한 이메일 주소를 입력하세요.")
 
     class Meta:
         model = User
-        fields = ['name', 'username', 'password1', 'password2', 'email']
+        fields = ["name", "username", "password1", "password2", "email"]
+
 
 class FindUsernameForm(forms.Form):
-    email = forms.EmailField(label='이메일')
+    email = forms.EmailField(label="이메일")
