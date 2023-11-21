eeb741126872aca90643757eb13b380eaa5f8e3a
devdnwl <133637966+devdnwl@users.noreply.github.com>
2023-11-21 09:13:47 +0900

    11월 21일

--- board_page/urls.py
+++ board_page/urls.py
@@ -1,21 +1,13 @@
 from django.urls import path
 from . import views
-<<<<<<< HEAD
 from .views import board, create_post, post_detail, edit_post, delete_post
-=======
-from .views import board, create_post, delete_post
->>>>>>> eeb6e9f7fb30e90dea0759d077c4b75b2f9bc960
 
 urlpatterns = [
     path("board/", board, name="board"),
     path("create_post/", create_post, name="create_post"),
-<<<<<<< HEAD
     path('post/<int:post_id>/', post_detail, name='post_detail'),
     path('edit_post/<int:post_id>/', edit_post, name='edit_post'),
     path('delete_post/<int:post_id>/', delete_post, name='delete_post'),
 
 
-=======
-    path("delete_post/<int:post_id>/", delete_post, name="delete_post"),
->>>>>>> eeb6e9f7fb30e90dea0759d077c4b75b2f9bc960
 ]
--- board_page/views.py
+++ board_page/views.py
@@ -6,13 +6,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def board(request):
-<<<<<<< HEAD
-    posts = posts = Post.objects.all().order_by('-created_at')
-    return render(request, "board.html", {"posts": posts})
-=======
     posts = Post.objects.all()
     return render(request, "board.html", {"posts": posts, "user": request.user})
->>>>>>> eeb6e9f7fb30e90dea0759d077c4b75b2f9bc960
 
 
 @login_required
@@ -29,7 +24,6 @@ def create_post(request):
 
     return render(request, "create_post.html", {"form": form})
 
-<<<<<<< HEAD
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     return render(request, 'post_detail.html', {'post': post})
@@ -58,15 +52,3 @@ def download_audio(request, post_id):
     response = HttpResponse(post.audio_file.read(), content_type='audio/mpeg')
     response['Content-Disposition'] = f'attachment; filename="{post.audio_file.name}"'
     return response
-=======
-
-@login_required
-def delete_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-
-    # Check if the current user has permission to delete the post
-    if request.user == post.author or request.user.is_superuser:
-        post.delete()
-
-    return redirect("board")
->>>>>>> eeb6e9f7fb30e90dea0759d077c4b75b2f9bc960
