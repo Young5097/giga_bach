4249b3bd0c9dbf64a090794242d3b61b8ffeaf6e
devdnwl <133637966+devdnwl@users.noreply.github.com>
2023-12-07 20:57:07 +0900

    getmusic확인

---
+++ ms_page/getmusic.py
@@ -0,0 +1,41 @@
+import os
+import miditoolkit
+from pydub import AudioSegment
+import librosa
+import librosa.display
+from midi2audio import FluidSynth
+from IPython.display import Audio, display
+import subprocess
+
+
+def generate_music(load_path, file_path):
+    directory_path = '/content/drive/MyDrive/giga_bach/getmusic'
+    os.chdir(directory_path)
+    command = f'python track_generation.py --load_path {load_path} --file_path {file_path}'
+    subprocess.run(command, shell=True)
+
+
+def convert_midi_to_wav(input_folder, output_folder):
+    fs = FluidSynth()
+
+    for filename in os.listdir(input_folder):
+        if filename.endswith(".midi") or filename.endswith(".mid"):
+            midi_path = os.path.join(input_folder, filename)
+            wav_path = os.path.join(output_folder, os.path.splitext(filename)[0] + ".wav")
+            fs.midi_to_audio(midi_path, wav_path)
+
+    print("모든 미디 파일을 WAV 파일로 변환하였습니다.")
+
+
+def play_audio_files(audio_folder):
+    supported_formats = ['.wav', '.mp3', '.ogg', '.flac']
+
+    for filename in os.listdir(audio_folder):
+        if any(filename.endswith(format) for format in supported_formats):
+            file_path = os.path.join(audio_folder, filename)
+            display(Audio(file_path, autoplay=True))
+
+
+generate_music('/content/drive/MyDrive/giga_bach/checkpoint.pth', '/content/drive/MyDrive/giga_bach/APTITUDE/media/got_temp_midi')
+convert_midi_to_wav('/content/drive/MyDrive/giga_bach/APTITUDE/media/got_temp_midi', '/content/drive/MyDrive/giga_bach/APTITUDE/media/midi2wav')
+play_audio_files('/content/drive/MyDrive/giga_bach/APTITUDE/media/midi2wav')
--- ms_page/views.py
+++ ms_page/views.py
@@ -4,12 +4,13 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404
 from django.conf import settings
 from .forms import SongForm
-from .sound2midi import sound2midi
+from .sound2to2midi import sound2midi
 from .midi_catcher import identify_instruments
 import os
 
+
 def make_song(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = SongForm(request.POST, request.FILES)
         if form.is_valid():
             song = form.save()
@@ -17,7 +18,7 @@ def make_song(request):
             audio_file_path = song.audio_file.path
 
             # 음악 파일이 MIDI 확장자인지 확인
-            if audio_file_path.endswith('.midi') or audio_file_path.endswith('.mid'):
+            if audio_file_path.endswith(".midi") or audio_file_path.endswith(".mid"):
                 identify_instruments(audio_file_path)
             else:
                 sound2midi(audio_file_path)
@@ -27,26 +28,31 @@ def make_song(request):
     else:
         form = SongForm()
 
-    return render(request, 'make_song.html', {'form': form})
+    return render(request, "make_song.html", {"form": form})
+
 
 def ms_result(request):
     # 세션에서 MIDI 파일의 경로를 가져옴
-    midi_file_path = request.session.get('midi_file_path')
+    midi_file_path = "./APTITUDE/media/got_temp_midi/outputs.mid"
 
     if midi_file_path:
-        return render(request, 'ms_result.html', {'midi_file_path': midi_file_path})
+        return render(request, "ms_result.html", {"midi_file_path": midi_file_path})
     else:
         return HttpResponse("Error: MIDI file not found.")
-    
+
+
 def download_midi(request):
     # 세션에서 MIDI 파일의 경로를 가져옴
-    midi_file_path = request.session.get('midi_file_path')
+    midi_file_path = "./APTITUDE/media/got_temp_midi/outputs.mid"
 
     if midi_file_path:
         # MIDI 파일이 존재하면 파일을 읽어서 응답으로 반환
-        with open(midi_file_path, 'rb') as midi_file:
-            response = HttpResponse(midi_file.read(), content_type='audio/midi')
-            response['Content-Disposition'] = f'attachment; filename={os.path.basename(midi_file_path)}'
+        with open(midi_file_path, "rb") as midi_file:
+            response = HttpResponse(midi_file.read(), content_type="audio/midi")
+            response[
+                "Content-Disposition"
+            ] = f"attachment; filename={os.path.basename(midi_file_path)}"
             return response
     else:
         return HttpResponse("Error: MIDI file not found.")
+
